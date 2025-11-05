844c1bd03d23e8924bcb4a1e28de7f191a65f3ce
Vegadjay <jayvegad10@gmail.com>
2025-11-05 12:54:09 +0530

    update project

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,12 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!;
-
-if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
-  );
-}
+const MONGODB_URI = process.env.MONGODB_URI;
 
 interface MongooseCache {
   conn: typeof mongoose | null;
@@ -26,6 +20,12 @@ if (!global.mongoose) {
 async function connectDB(): Promise<typeof mongoose> {
   if (cached.conn) {
     return cached.conn;
+  }
+
+  if (!MONGODB_URI || MONGODB_URI.trim() === "") {
+    throw new Error(
+      "Please define the MONGODB_URI environment variable inside .env.local"
+    );
   }
 
   if (!cached.promise) {
